7cf541b24220f3e9f396ed4f71dd58a7a10183b0
Vasyl Fomiuk <vasyl@cybertekschool.com>
2021-01-03 11:48:50 -0500

    added chewy test class

--- src/test/java/com/cybertek/tests/ChewyTests.java
+++ src/test/java/com/cybertek/tests/ChewyTests.java
@@ -1,46 +1,42 @@
 package com.cybertek.tests;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.Platform;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+
 import java.net.URL;
 
-public class ChromeBrowserTest {
+public class ChewyTests {
 
-    WebDriver driver;
+    AppiumDriver<AndroidElement> driver;
 
     @Before
     public void setup() throws Exception {
-        //desiredCapabilities.setCapability("platformName", "Android");
-        //to specify test settings and required info about device and app under the test
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.0");
         desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
         desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-        desiredCapabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, "https://cybertek-appium.s3.amazonaws.com/chewy.apk");
 
         URL url = new URL("http://localhost:4723/wd/hub");
-        driver = new RemoteWebDriver(url, desiredCapabilities);
+        driver = new AndroidDriver<>(url, desiredCapabilities);
     }
 
     @Test
-    public void mobileChromeTest() throws InterruptedException {
-        driver.get("http://qa1.vytrack.com");
-        driver.findElement(By.id("prependedInput")).sendKeys("storemanager85");
-        driver.findElement(By.id("prependedInput2")).sendKeys("UserUser123", Keys.ENTER);
+    public void test1() throws InterruptedException {
         Thread.sleep(3000);
     }
 
     @After
     public void tearDown(){
-        driver.quit();
+        driver.closeApp();
     }
 }
