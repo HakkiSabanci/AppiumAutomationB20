5255c6212b18dd08d6b5a3391cb2dec589dc9354
Vasyl Fomiuk <vasyl@cybertekschool.com>
2021-01-03 12:49:08 -0500

    Update MobileUtils.java

--- src/test/java/com/cybertek/utils/MobileUtils.java
+++ src/test/java/com/cybertek/utils/MobileUtils.java
@@ -10,6 +10,64 @@ import java.time.Duration;
 
 public class MobileUtils {
 
+    public static void swipeScreen(Direction dir, AppiumDriver driver) {
+        System.out.println("swipeScreen(): dir: '" + dir + "'"); // always log your actions
+
+        // Animation default time:
+        //  - Android: 300 ms
+        //  - iOS: 200 ms
+        // final value depends on your app and could be greater
+        final int ANIMATION_TIME = 200; // ms
+
+        final int PRESS_TIME = 200; // ms
+
+        int edgeBorder = 10; // better avoid edges
+        PointOption pointOptionStart, pointOptionEnd;
+
+        // init screen variables
+        Dimension dims = driver.manage().window().getSize();
+
+        // init start point = center of screen
+        pointOptionStart = PointOption.point(dims.width / 2, dims.height / 2);
+
+        switch (dir) {
+            case DOWN: // center of footer
+                pointOptionEnd = PointOption.point(dims.width / 2, dims.height - edgeBorder);
+                break;
+            case UP: // center of header
+                pointOptionEnd = PointOption.point(dims.width / 2, edgeBorder);
+                break;
+            case LEFT: // center of left side
+                pointOptionEnd = PointOption.point(edgeBorder, dims.height / 2);
+                break;
+            case RIGHT: // center of right side
+                pointOptionEnd = PointOption.point(dims.width - edgeBorder, dims.height / 2);
+                break;
+            default:
+                throw new IllegalArgumentException("swipeScreen(): dir: '" + dir + "' NOT supported");
+        }
+
+        // execute swipe using TouchAction
+        try {
+            new TouchAction(driver)
+                    .press(pointOptionStart)
+                    // a bit more reliable when we add small wait
+                    .waitAction(WaitOptions.waitOptions(Duration.ofMillis(PRESS_TIME)))
+                    .moveTo(pointOptionEnd)
+                    .release().perform();
+        } catch (Exception e) {
+            System.err.println("swipeScreen(): TouchAction FAILED\n" + e.getMessage());
+            return;
+        }
+
+        // always allow swipe action to complete
+        try {
+            Thread.sleep(ANIMATION_TIME);
+        } catch (InterruptedException e) {
+            // ignore
+        }
+    }
+
     public static void swipeScreenSmall(Direction dir, AppiumDriver driver) {
         System.out.println("swipeScreenSmall(): dir: '" + dir + "'"); // always log your actions
 
